987106586e1a254ce991e811d1325363d54ccfe8
ilhamramadhan4 <ilham@medigo.id>
2020-01-11 19:42:32 +0700

    #Add card for company description

--- pages/index.vue
+++ pages/index.vue
@@ -2,80 +2,69 @@
   <v-layout
     column
     justify-center
-    align-center
   >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-layout d-flex align-start>
+            <h1>Our tagline goes here!!</h1>
+          </v-layout>
+        </v-col>
+        <v-col>
+          <v-layout align-end justify-end>
+            <img
+              src="/v.png"
+              alt="Vuetify.js"
+              class="mb-5"
+            />
+          </v-layout>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-card
+            class="mx-auto"
+            max-width="600"
+            outlined
           >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
+            <v-list-item three-line>
+            <v-list-item-content>
+                <v-list-item-title class="headline mb-1">Innovative courses with lots variety of subject</v-list-item-title>
+                <v-card-text>
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                </v-card-text>
+              </v-list-item-content>
+            </v-list-item>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card
+            class="mx-auto"
+            max-width="600"
+            outlined
           >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
+            <v-list-item three-line>
+            <v-list-item-content>
+                <v-list-item-title class="headline mb-1">Innovative courses with lots variety of subject</v-list-item-title>
+                <v-card-text>
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                </v-card-text>
+              </v-list-item-content>
+            </v-list-item>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row>
+        <h1>Our tagline goes here!!</h1>
+      </v-row>
+      <v-row>
+        <h1>Our tagline goes here!!</h1>
+      </v-row>
+    </v-container>
   </v-layout>
 </template>
 
